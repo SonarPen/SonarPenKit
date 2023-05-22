5fb430bfc652f2ad27a054e894b531c24417f6dc
Water Lou <water@waterworld.com.hk>
2023-05-23 00:18:02 +0800

    not compile

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SonarPenKit",
+    name: "SonarPenKitPackage",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SonarPenKit",
+            name: "SonarPenKitPackage",
             targets: ["SonarPenKit"]),
     ],
     dependencies: [
