b763f438115056c14791bc62915bb9226f7005a8
Water Lou <water@waterworld.com.hk>
2023-05-23 09:37:58 +0800

    test

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "SonarPenKitPackage",
+    name: "SonarPenKit",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SonarPenKitPackage",
+            name: "SonarPenKit",
             targets: ["SonarPenKit"]),
     ],
     dependencies: [
