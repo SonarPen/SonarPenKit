8e0fd94b64a404bd3039c3efd5736c0d55c5e335
Water Lou <water@waterworld.com.hk>
2023-05-23 09:46:34 +0800

    remove unused code

--- Package.swift
+++ Package.swift
@@ -12,23 +12,13 @@ let package = Package(
             name: "SonarPenKit",
             targets: ["SonarPenKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "SonarPenKit",
-//            dependencies: []),
         .binaryTarget(
                     name: "SonarPenKit",
                     url: "https://github.com/SonarPen/SonarPenKit/raw/main/Bin/SonarPenKit.xcframework.zip",
                     checksum: "bea7a941e12f4586db84be9bfab59710bef87ceaeac46644d2c0572f8c5347ce"
                 ),
-//        .testTarget(
-//            name: "SonarPenKitTests",
-//            dependencies: ["SonarPenKit"]),
     ]
 )
--- Sources/SonarPenKit/SonarPenKit.swift
+++
@@ -1,9 +0,0 @@
-import SonarPenKitBinaryPackage
-
-public struct SonarPenKit {
-    
-    public static func start(application: UIApplication) {
-        SonarPenKitBinaryPackage.SonarPenKit.start(application: application)
-    }
-    
-}
